e35e5a1f7e70ff402937e7585989b36e8a30d57e
Scott <ssulivan11@gmail.com>
2014-01-21 22:03:55 -0600

    yo angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('sjSportfolioSiteApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
